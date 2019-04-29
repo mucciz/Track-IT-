03aae90366742d9c73f6af28b13063b1fe35a32a
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-04-29 07:49:58 +0300

    Intialized all the JavaScript variables

--- track.js
+++ track.js
@@ -1,4 +1,7 @@
 //create the function that'll compare user input and give a verdict
 function track () {
-    
+    //Initalize variables where JavaScript will 'place' the values it gets from the form inputs.
+    var length1 = parseInt (document.getElementById('first').value);
+    var length2 = parseInt (document.getElementById('second').value);
+    var length3 = parseInt (document.getElementById('third').value);
 }

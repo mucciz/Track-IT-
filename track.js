5f440b8227b653568fb4bf9d135629081e105cd0
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-04-29 07:51:31 +0300

    Added an array to hold the variables

--- track.js
+++ track.js
@@ -4,4 +4,6 @@ function track () {
     var length1 = parseInt (document.getElementById('first').value);
     var length2 = parseInt (document.getElementById('second').value);
     var length3 = parseInt (document.getElementById('third').value);
+    //store the variables in array so that we use their positions in the array to access them. Not really neccessary but it works.
+    var someArray = [length1,length2,length3];
 }
